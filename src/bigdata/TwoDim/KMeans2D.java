f7cea49d45d6838d0599986765b30376af18b7ec
Matthias Duffour <matthias.duffour@gmail.com>
2016-11-30 14:23:27 +0100

    Adding everything (really this time)

---
+++ src/bigdata/TwoDim/KMeans2D.java
@@ -0,0 +1,23 @@
+package bigdata.TwoDim;
+/**
+ * @author David Auber 
+ * @date 07/10/2016
+ * Ma�tre de conf�rences HDR
+ * LaBRI: Universit� de Bordeaux
+ */
+
+import org.apache.hadoop.util.ProgramDriver;
+
+public class KMeans2D {
+	
+	public static void main( String[] args ) throws Exception {
+		ProgramDriver pgd = new ProgramDriver();
+		int exitCode = -1;
+		try {
+			exitCode = pgd.run(args);
+		} catch (Throwable e1)  {
+			e1.printStackTrace();
+		}
+		System.exit(exitCode);
+	}
+}
---
+++ src/bigdata/TwoDim/KMeans2DIt.java
@@ -0,0 +1,51 @@
+package bigdata.TwoDim;
+
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.util.Set;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.ToolRunner;
+
+
+public class KMeans2DIt{
+	public static class KMeans2DItMapper extends Mapper<NullWritable, Text, Set<Point2D>, DoubleWritable> {
+		public void map(NullWritable key, Text value, Context context) throws IOException, InterruptedException {
+			
+		}
+	}
+	
+	public static class KMeans2DItReducer extends Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Text value, Context context) throws IOException, InterruptedException {
+			
+		}
+	}
+
+	public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+	    Job job = Job.getInstance(conf, "Projet");
+	    job.setNumReduceTasks(1);
+	    job.setJarByClass(KMeans2DIt.class);
+	    job.setMapperClass(KMeans2DItMapper.class);
+	    job.setMapOutputKeyClass(Text.class);
+	    job.setMapOutputValueClass(Text.class);
+	    job.setReducerClass(KMeans2DItReducer.class);
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(Text.class);
+	    job.setOutputFormatClass(TextOutputFormat.class);
+	    //FileOutputFormat.setOutputPath(job, new Path(args[2]));
+	    return job.waitForCompletion(true) ? 0 : 1;
+	}
+	
+	public static void main(String args[]) throws Exception {
+		System.exit(ToolRunner.run(null, args));
+	}
+}
