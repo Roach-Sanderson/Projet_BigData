8407030d9cc2854d250573a00b5e63b34cd9a163
Matthias Duffour <matthias.duffour@gmail.com>
2017-01-14 23:24:54 +0100

    Hier began

--- src/bigdata/BigData.java
+++ src/bigdata/BigData.java
@@ -11,6 +11,7 @@ public class BigData {
 		try {
 			pgd.addClass("1d", bigdata.TwoDim.KMeans1DIt.class, "creates a kmeans");
 			pgd.addClass("nd", bigdata.TwoDim.KMeansND.class, "creates a kmeans");
+			pgd.addClass("hier", bigdata.TwoDim.KMeansHier.class, "creates a kmeans");
 			exitCode = pgd.run(args);
 		} catch (Throwable e1)  {
 			e1.printStackTrace();
